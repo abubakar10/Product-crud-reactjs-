588a0b87e0edd20d96d437b9a9668a25ee5e67c0
Malik Abubakar Shafeeq <163340586+abubakar10@users.noreply.github.com>
2025-11-18 10:50:16 +0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders Shop Now button', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  const linkElement = screen.getByText(/shop now/i);
   expect(linkElement).toBeInTheDocument();
 });
+
