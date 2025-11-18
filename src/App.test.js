7b17569492056951817e72f76a055072d06dbe0c
Malik Abubakar Shafeeq <163340586+abubakar10@users.noreply.github.com>
2025-11-18 10:55:53 +0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
-test('renders Shop Now button', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-
-  const linkElement = screen.getByText(/shop now/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
+import { render, screen } from '@testing-library/react'; 
+import App from './App'; 
+test('renders learn react link', () => 
+  { render(<App />); const linkElement = screen.getByText(/shop now/i);
+                                                
+ expect(linkElement).toBeInTheDocument(); });
